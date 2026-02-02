22c2908c57f50e11918d9a2150cf4c5975781f22
Samin <saminsoltanieh@gmail.com>
2026-02-02 14:10:56 +0330

    :add foreign key

--- models/seat.go
+++ models/seat.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Seat struct {
 	gorm.Model
 	HallID   uint   `json:"hall_id"`
-	Hall     Hall   `gorm:"foreignKey:HallID"`
+	Hall     Hall   `gorm:"foreignKey:HallID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT"`
 	Row      string `json:"row"` //abcd
 	Number   int    `json:"number"`
 	IsActive bool   `json:"is_active"`
