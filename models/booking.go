19607217cd03a4d605d56a807f4472242cf01b2a
Samin <saminsoltanieh@gmail.com>
2026-02-02 14:09:54 +0330

    :add foreign key

--- models/booking.go
+++ models/booking.go
@@ -4,11 +4,11 @@ import "gorm.io/gorm"
 
 type Booking struct {
 	gorm.Model
-	UserID     uint     `json:"user_id"`
-	User       User     `json:"user"`
-	SeatID     uint     `json:"seat_id"`
-	Seat       Seat     `json:"seat"`
-	ShowtimeID uint     `json:"showtime_id"`
-	Showtime   Showtime `json:"showtime"`
-	Status     string   `json:"status"` //reserved/cancel
+	UserID     uint     `json:"user_id" gorm:"not null"`
+	User       User     `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	SeatID     uint     `json:"seat_id" gorm:"not null"`
+	Seat       Seat     `gorm:"foreignKey:SeatID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	ShowtimeID uint     `json:"showtime_id" gorm:"not null"`
+	Showtime   Showtime `gorm:"foreignKey:ShowtimeID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Status     string   `json:"status" gorm:"check:status IN ('reserved','cancel')"` //reserved/cancel
 }
